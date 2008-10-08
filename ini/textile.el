0ba7985ac1a452b343e2c22118487a9206840eaa
Jim Weirich <jim.weirich@gmail.com>
2008-10-08 15:09:31 -0400

    textile and blogging support.

--- ini/textile.el
+++ ini/textile.el
@@ -7,5 +7,11 @@
 
 (require 'textile-mode)
 
+;; The default link face is too dark for a black background.
+(defface textile-link-face
+  '((t (:foreground "LightBlue")))
+  "Face used to highlight links."
+  :group 'textile-faces)
+
 (add-to-list 'auto-mode-alist '("\\.textile\\'" . textile-mode))
 (add-to-list 'auto-mode-alist '("\\.red\\'" . textile-mode))
