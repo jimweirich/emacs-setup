8bb0d434f18c0b4ba103311af76160227cdfe9ac
Jim Weirich <jim.weirich@gmail.com>
2008-11-20 23:12:34 -0500

    lightened up the rails <% %> delimiters

--- ini/rails.el
+++ ini/rails.el
@@ -17,6 +17,16 @@
   "Default inherited face for ERB tag delimeters"
   :group 'rhtml-faces)
 
+(defface erb-out-delim-face
+  `((t (:inherit erb-delim-face :weight bold :foreground "yellow")))
+  "Basic face for Ruby embedded into HTML"
+  :group 'rhtml-faces)
+
+(defface erb-exec-delim-face
+  `((t (:inherit erb-delim-face :weight bold :foreground "yellow")))
+  "Basic face for Ruby embedded into HTML"
+  :group 'rhtml-faces)
+
 ;;; I don't like the rinari abbrevs, so ignore them
 (provide 'rinari-abbrevs)
 (require 'rinari)
