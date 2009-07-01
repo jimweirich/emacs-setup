7c1bec1498cc22331bc7fcd8fb8e0f3308fdbb8f
Jim Weirich <jim.weirich@gmail.com>
2009-07-01 18:32:10 -0400

    Added paredit

---
+++ ini/paredit.el
@@ -0,0 +1,9 @@
+;;; Paredit setup
+
+(require 'paredit)
+
+(mapc (lambda (mode)
+	(let ((hook (intern (concat (symbol-name mode)
+				    "-mode-hook"))))
+	  (add-hook hook (lambda () (paredit-mode +1)))))
+      '(emacs-lisp lisp inferior-lisp clojure))
