771d5dfede0ae576e989d759128cdc72718e63a9
Jim Weirich <jim.weirich@gmail.com>
2010-07-15 10:05:41 -0400

    Minor code formatting.

--- ini/magit-setup.el
+++ ini/magit-setup.el
@@ -1,6 +1,5 @@
-;; (add-to-load-path (concat elisp-directory "/pkgs/magit/etc/emacs/site-start.d/"))
+;; Magit setup
+
 (add-to-load-path (concat elisp-directory "/pkgs/magit/share/emacs/site-lisp"))
-
 (autoload 'magit-status "magit" nil t)
-
 (setq magit-git-executable "/usr/local/bin/git")
