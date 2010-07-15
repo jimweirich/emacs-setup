c6845e5cdc95fb47f512c60d17bbeba3a8b98f93
Jim Weirich <jim.weirich@gmail.com>
2010-07-15 10:02:56 -0400

    Removed obsolete testing related key bindings.

--- ini/keybindings.el
+++ ini/keybindings.el
@@ -71,11 +71,6 @@
 (global-set-key (kbd "C-C t C-t") 'jw-toggle-clear-buffer-styles) ; pkg:testing
 (global-set-key (kbd "C-C t C-s") 'toggle-debug) ; pkg:testing
 
-(global-set-key (kbd "C-C t y") 'jw-testing-use-ruby)
-(global-set-key (kbd "C-C t j") 'jw-testing-use-jruby)
-(global-set-key (kbd "C-C t 9") 'jw-testing-use-ruby19)
-(global-set-key (kbd "C-C t x") 'jw-testing-use-ruby19x)
-
 (global-set-key (kbd "C-C t 1") (lambda () (interactive) (setq jw-test-single-window t))) ; pkg:testing
 (global-set-key (kbd "C-C t 2") (lambda () (interactive) (setq jw-test-single-window nil))) ; pkg:testing
 
