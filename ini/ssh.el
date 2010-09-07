c845e7c3aa27fd4c3e94baf7e87edfc482509bed
Jim Weirich <jim.weirich@gmail.com>
2010-09-06 22:30:36 -0400

    Changed ssh agent short-cut to 'ra' to match shell.

--- ini/ssh.el
+++ ini/ssh.el
@@ -1,11 +1,12 @@
-(defun jw-reload-ssh-env ()
+(defun jw-reload-ssh-agent-env ()
   "Reload the SSH environment if it exists."
   (interactive)
   (let ((env-file (expand-file-name "~/.ssh/environment.el")))
     (if (file-readable-p env-file)
         (load-file env-file))))
 
-(defun rssh ()
-  "Short-cut for jw-reload-ssh-env."
+(defun ra ()
+  "Short-cut for jw-reload-ssh-agent-env."
   (interactive)
   (jw-reload-ssh-env))
+
