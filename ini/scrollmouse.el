252108a2a49d415ca707695299c06a3928c7d1b6
Jim Weirich <jim.weirich@gmail.com>
2011-08-31 20:43:25 -0600

    Smaller mouse scrolling increments

--- ini/scrollmouse.el
+++ ini/scrollmouse.el
@@ -5,34 +5,36 @@
 ;;; ==================================================================
 
 
-(cond 
+(cond
  ((is-xemacs)
   (defun jw-mouse-scroll-amount ()
     (if (is-aquamacs)
         (max 1 (window-displayed-height) 3)
       (max 1 (/ (window-displayed-height) 3)) ))
-  
+
   (defun jw-blip-down ()
     (interactive)
     (scroll-down-command (jw-mouse-scroll-amount)))
-  
+
   (defun jw-blip-up ()
     (interactive)
     (scroll-up-command (jw-mouse-scroll-amount)))
-  
+
   (define-key global-map [button4] 'jw-blip-down)
   (define-key global-map [button5] 'jw-blip-up) )
  (t
   (defun jw-mouse-scroll-amount ()
     (max 1 (/ (window-height) 5)))
-  
+
   (defun jw-blip-down ()
     (interactive)
     (scroll-down (jw-mouse-scroll-amount)))
-  
+
   (defun jw-blip-up ()
     (interactive)
     (scroll-up (jw-mouse-scroll-amount)))
-  
+
   (define-key global-map [mouse-4] 'jw-blip-down)
   (define-key global-map [mouse-5] 'jw-blip-up) ))
+
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 5) ((control) . 10)))
